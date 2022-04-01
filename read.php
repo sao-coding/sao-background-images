bc9f0bb7321b7dffd63cbe61bbc5c3ec58f9657a
sao-coding <58339015+sao-coding@users.noreply.github.com>
2022-04-02 01:50:36 +0800

    Delete read.php

--- read.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('IMAGEPATH', 'img/');
-foreach(glob(IMAGEPATH.'*.{jpg,JPG,jpeg,JPEG,png,PNG}',GLOB_BRACE) as $filename){
-    $images[] =  basename($filename);
-}
-$rand_key = array_rand($images, 1);
-?>
